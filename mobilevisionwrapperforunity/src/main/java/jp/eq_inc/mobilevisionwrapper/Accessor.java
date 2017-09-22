144a63a1ce9e34f6276aafb409f7895977c4d309
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-09-22 14:06:21 +0900

    そこそこ動作するようになった気がする版

--- mobilevisionwrapperforunity/src/main/java/jp/eq_inc/mobilevisionwrapper/Accessor.java
+++ mobilevisionwrapperforunity/src/main/java/jp/eq_inc/mobilevisionwrapper/Accessor.java
@@ -1,9 +1,6 @@
 package jp.eq_inc.mobilevisionwrapper;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.Rect;
-import android.graphics.YuvImage;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceView;
@@ -17,9 +14,6 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 import com.google.android.gms.vision.text.TextRecognizer;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import jp.eq_inc.mobilevision.detector.AllDetector;
@@ -161,9 +155,7 @@ public abstract class Accessor {
                                 synchronized (AccessorForUnity.this) {
                                     if (mImageBuffer == null) {
                                         try {
-                                            Log.d(AccessorForUnity.class.getSimpleName(), "sleep in");
                                             AccessorForUnity.this.wait();
-                                            Log.d(AccessorForUnity.class.getSimpleName(), "sleep out");
                                         } catch (InterruptedException e) {
                                         }
                                     }
@@ -179,40 +171,23 @@ public abstract class Accessor {
 
                                 if (imageBuffer != null) {
                                     SparseArray ret = null;
-                                    Frame.Builder frameBuilder = new Frame.Builder();
-                                    frameBuilder.setImageData(ByteBuffer.wrap(imageBuffer), imageWidth, imageHeight, imageFormat);
-                                    Frame frame = frameBuilder.build();
 
-                                    if (frameIndex < 100) {
-                                        YuvImage tempYuvImage = new YuvImage(imageBuffer, imageFormat, imageWidth, imageHeight, imageStride);
+                                    try {
+                                        Frame.Builder frameBuilder = new Frame.Builder();
+                                        frameBuilder.setImageData(ByteBuffer.wrap(imageBuffer), imageWidth, imageHeight, imageFormat);
+                                        Frame frame = frameBuilder.build();
 
-                                        if (tempYuvImage != null) {
-                                            FileOutputStream outStream = null;
-
-                                            try {
-                                                outStream = new FileOutputStream(String.format("/mnt/sdcard/frame_%04d.png", frameIndex));
-                                                tempYuvImage.compressToJpeg(new Rect(0, 0, imageWidth, imageHeight), 100, outStream);
-                                            } catch (FileNotFoundException e) {
-                                                e.printStackTrace();
-                                            } finally {
-                                                if (outStream != null) {
-                                                    try {
-                                                        outStream.close();
-                                                    } catch (IOException e) {
-                                                        e.printStackTrace();
-                                                    }
-                                                    outStream = null;
-                                                }
+                                        for (int i = 0; i < detectorTypeCount; i++) {
+                                            if (detectorArray[i] != null) {
+                                                ret = detectorArray[i].detect(frame);
+                                                listenerArray[i].onDetected(ret);
                                             }
                                         }
+                                    } finally {
+
                                     }
 
-                                    for (int i = 0; i < detectorTypeCount; i++) {
-                                        if (detectorArray[i] != null) {
-                                            ret = detectorArray[i].detect(frame);
-                                            listenerArray[i].onDetected(ret);
-                                        }
-                                    }
+                                    frameIndex++;
                                 }
                             }
                         }
